742bf45d1ce814bd3683c8f5aee1385a1726c95c
Javier Martínez <143547061+Javierjmc@users.noreply.github.com>
2025-11-27 10:50:45 -0400

    Update report month from September to November

--- cliente/src/components/pdf/MyDocument.jsx
+++ cliente/src/components/pdf/MyDocument.jsx
@@ -228,7 +228,7 @@ const MyDocument = ({ report = {} }) => (
             DEPARTAMENTO DE CALIDAD
           </Text>
           <Text style={styles.headerText}>
-            Reporte Mensual Septiembre {report.texto}
+            Reporte Mensual Noviembre {report.texto}
           </Text>
         </View>
       </View>
